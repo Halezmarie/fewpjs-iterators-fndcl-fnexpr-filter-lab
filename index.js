a4ef94aeee14aa9bbb6eebae509b9894edf48495
halezmarie <teacherhaleymarie@gmail.com>
2021-08-23 02:07:19 -0500

    commit tree ad4314a3b55e7e59017f7d380b64bf4dd7866717

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+function findMatching(source, sought) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === sought.toLowerCase()
+    )
+  }
+  
+
+// ^ same as above, except it is a lot neater to do the arrow function
+
+//   function findMatching(drivers, string) {
+//     const matchingDrivers = drivers.filter(function(driver) {
+//         return driver.toUpperCase() === string.toUpperCase();
+//     });
+//     return matchingDrivers;
+// }
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+  }
